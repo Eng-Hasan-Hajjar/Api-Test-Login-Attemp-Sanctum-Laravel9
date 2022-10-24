f3da4a738d1354ac3226a43b06ac9bc1696275b3
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2022-10-24 06:21:52 -0700

    update1 vue with ..

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,23 @@
 import './bootstrap';
+import '../css/app.css';
 
-import Alpine from 'alpinejs';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-window.Alpine = Alpine;
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
-Alpine.start();
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
