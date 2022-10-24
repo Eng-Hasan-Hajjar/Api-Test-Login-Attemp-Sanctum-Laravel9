56ad5b067d59833b0237be9a1ba3145ce45b6555
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2022-10-24 10:46:28 -0700

    update2 add controllers models migrations packages routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,22 @@
 <?php
-
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       /* $this->call([
+            'PermissionsTableSeeder',
+            RolesTableSeeder::class,
+            PermissionRoleTableSeeder::class,
+            UsersTableSeeder::class,
+            RoleUserTableSeeder::class,
+        ]);*/
+       $this->call(RolesTableSeeder::class);
+       $this->call(PermissionsTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RoleUserTableSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+    // Route where we post our login
+    Route::post('login', 'LoginController@login');
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        // Logout the user
+        Route::post('logout', 'LoginController@logout');
+
+        // Get current logged in user
+        Route::get('user', 'AuthenticationController@user');
+
+        // Permissions
+        Route::apiResource('permissions', 'PermissionsApiController');
+
+        // Roles
+        Route::apiResource('roles', 'RolesApiController');
+
+        // Users
+        Route::apiResource('users', 'UsersApiController');
+
+    });
 });
